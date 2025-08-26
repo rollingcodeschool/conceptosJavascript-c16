0d694d4aec42049059a24775ec6791d69f28a2a8
Emilse Arias <emilsearias@gmail.com>
2025-08-26 11:20:30 -0300

    agregamos un ejemplo de comentarios y como mostrar mensajes

---
+++ js/01-variables.js
@@ -0,0 +1,21 @@
+// Esto es un comentario de una linea
+
+/*
+Esto es un comentario 
+de
+multiples lineas
+*/
+
+// Como mandar mensajes
+console.log("hola mundo");
+console.info("hola mundo");
+console.warn("hola mundo");
+console.error("hola mundo");
+
+// mostrar algo en el html
+document.writeln("hola mundo");
+document.writeln("<h1 class='text-blue'>hola mundo</h1>");
+
+// ventanas emergentes
+//alert("practica de alert")
+
