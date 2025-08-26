11ae9f8b87b8eb591bfddff8cb9552f34dac548d
Emilse Arias <emilsearias@gmail.com>
2025-08-26 12:00:53 -0300

    agregamos un ejemplo de como crear una variable constante y mostrarla por consola y en pantalla

--- js/01-variables.js
+++ js/01-variables.js
@@ -19,3 +19,11 @@ document.writeln("<h1 class='text-blue'>hola mundo</h1>");
 // ventanas emergentes
 //alert("practica de alert")
 
+// variables var - let - const
+
+const taiga = 'https://tree.taiga.io/project/earias08-comision-web16/us/74?kanban-include_attachments=1&kanban-include_tasks=1&kanban-status=10073551'
+
+console.log(taiga)
+document.writeln('<p>'+taiga+'</p>')
+document.writeln(taiga)
+document.writeln('un nuevo texto')
