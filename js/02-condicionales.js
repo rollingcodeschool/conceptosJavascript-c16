3ac9f11b1d246316bc20f5162641e0a1d8528c01
Emilse Arias <emilsearias@gmail.com>
2025-08-28 11:14:18 -0300

    controlamos la ejecucion del programa cuando nos devuelve un NaN

--- js/02-condicionales.js
+++ js/02-condicionales.js
@@ -3,7 +3,7 @@
 if(condicion){
     codigo a ejecutar si se cumple la condicion
 }
-*/ 
+*/
 
 // condicionales dobles
 /*
@@ -12,16 +12,26 @@ if(condicion){
 }else{
     codigo a ejecutar si no se cumple la condicion
 }
-*/ 
+*/
 
 // Pedir al usuario una edad y decir si es mayor de edad o no
 // 18 años es mayor de edad
 
-const edad = parseInt(prompt('Ingresa tu edad'))
-console.log(edad)
+const edad = parseInt(prompt("Ingresa tu edad"));
+console.log(edad);
 // > =
-if( edad >= 18){
-    document.writeln('Sos mayor de edad 😎')
-}else{
-    document.writeln('Sos menor de edad 😁')
+console.log(isNaN("hola")); //true
+console.log(isNaN("18"));
+console.log(isNaN(18));
+// if(isNaN(edad) === true)
+
+if (isNaN(edad)) {
+  document.writeln("Edad no válida");
+} else {
+  // aqui estoy segura que estoy trabajando con un numero
+  if (edad >= 18) {
+    document.writeln("Sos mayor de edad 😎");
+  } else {
+    document.writeln("Sos menor de edad 😁");
+  }
 }
