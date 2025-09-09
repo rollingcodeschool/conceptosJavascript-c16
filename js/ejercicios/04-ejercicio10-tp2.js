70c9ee2d139751fab08bf33357e9add6825f2ae7
Emilse Arias <emilsearias@gmail.com>
2025-09-09 11:01:36 -0300

    agregamos el valor de las celdas a la tabla

--- js/ejercicios/04-ejercicio10-tp2.js
+++ js/ejercicios/04-ejercicio10-tp2.js
@@ -2,13 +2,15 @@
 
 const filas = parseInt(prompt('Ingresa la cantidad de filas'))
 const columnas = parseInt(prompt('Ingresa la cantidad de columnas'))
+let total = filas * columnas
 
 document.writeln(`<table class="table table-striped-columns"><tbody>`)
 
 for(let indiceFilas=1; indiceFilas <= filas ; indiceFilas++){
 document.writeln(`<tr>`)
     for(let indiceColumnas=1; indiceColumnas <= columnas; indiceColumnas++){
-        document.writeln(`<td>1</td>`)
+        document.writeln(`<td>${total--}</td>`)
+        //total--; total = total - 1
     }
 document.writeln(`</tr>`)
 }
