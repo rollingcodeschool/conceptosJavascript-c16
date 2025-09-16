4338b04843796bc81dc29bb0adba9da8ab4b8db3
Emilse Arias <emilsearias@gmail.com>
2025-09-16 10:53:21 -0300

    agregamos ejemplo de sintaxis de expresion de funcion y arrow functions

--- js/06-funciones.js
+++ js/06-funciones.js
@@ -1,31 +1,43 @@
 // como declarar o crear una funcion tradicional
-function saludar(){
-    //aqui agrego todas las lineas de codigo que quiero aislar en esta funcion
-    document.writeln('<p>Hola mundo 🌎</p>')
-}
+// function saludar(){
+//     //aqui agrego todas las lineas de codigo que quiero aislar en esta funcion
+//     document.writeln('<p>Hola mundo 🌎</p>')
+// }
+
+// opcion de sintaxis: expression de funcion
+// const saludar = function (){
+//      document.writeln('<p>Hola mundo 🌎</p>')
+// }
+
+// opcion de sintaxis: arrow function
+const saludar = () => {
+  document.writeln("<p>Hola mundo 🌎</p>");
+};
 
 //funcion tradicional con parametros
-function valorAleatorio(max, min){
-   const aleatorio = Math.floor(Math.random() * (max - min + 1) + min);
-    document.writeln(`<p>El numero aleatorio creado es: ${aleatorio}</p>`)
+function valorAleatorio(max, min) {
+  const aleatorio = Math.floor(Math.random() * (max - min + 1) + min);
+  document.writeln(`<p>El numero aleatorio creado es: ${aleatorio}</p>`);
 }
 
 //funciones que retornan un valor
-function suma(numero1, numero2){
-    const total = numero1 + numero2
-    console.log(total)
-    return total
-}
+// function suma(numero1, numero2){
+//     const total = numero1 + numero2
+//     console.log(total)
+//     return total
+// }
+const suma = (numero1, numero2) => numero1 + numero2;
 
 
 //invocar o llamar a una funcion
-saludar()
+saludar();
 
-const max = parseInt(prompt('Ingresa un numero maximo'))
-const min = parseInt(prompt('Ingresa un numero minimo'))
+const max = parseInt(prompt("Ingresa un numero maximo"));
+const min = parseInt(prompt("Ingresa un numero minimo"));
 
-valorAleatorio(max,min)
+valorAleatorio(max, min);
 
-const resultado = suma(10,24);
-document.writeln(`<p>El resultado de la suma es: ${resultado}</p>`)
-document.writeln(`<p>El resultado de la suma es: ${suma(12,24)}</p>`)
+const resultado = suma(10, 24);
+document.writeln(`<p>El resultado de la suma es: ${resultado}</p>`);
+document.writeln(`<p>El resultado de la suma es: ${suma(12, 24)}</p>`);
+
