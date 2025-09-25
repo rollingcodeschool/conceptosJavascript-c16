3563689835c1dd556e62e7793566e26bc6ee2f80
Emilse Arias <emilsearias@gmail.com>
2025-09-25 11:16:51 -0300

    diseñamos la clase usuario con propiedades publicas

--- js/08-objetos.js
+++ js/08-objetos.js
@@ -14,68 +14,76 @@ cambiarEstado(nuevoEstado)
 */
 
 //objeto vacio
-const cancion = {}
+const cancion = {};
 
 //objeto con valores
 const funko = {
-    //definir las propiedades
-    //clave: valor (cualquier tipo de dato de js)
-    codigo : 867, 
-    precio: 20.5,
-    nombre: 'Tanjiro kamado',
-    marca: 'special edition',
-    estado: true,
-    //métodos
-    mostrarDatos: function (){
-        console.log(this)
-        document.writeln(`<p><b>Funko: ${this.nombre} </b></p>`)
-        document.writeln(`<ul>
+  //definir las propiedades
+  //clave: valor (cualquier tipo de dato de js)
+  codigo: 867,
+  precio: 20.5,
+  nombre: "Tanjiro kamado",
+  marca: "special edition",
+  estado: true,
+  //métodos
+  mostrarDatos: function () {
+    console.log(this);
+    document.writeln(`<p><b>Funko: ${this.nombre} </b></p>`);
+    document.writeln(`<ul>
             <li>Código: ${this.codigo} </li>
             <li>Precio: ${this.precio} </li>
             <li>Marca: ${this.marca} </li>
             <li>Estado: ${this.estado} </li>
-            </ul>`)
-    },
-    actualizarPrecio: function (nuevoPrecio) {
-        console.log(this)
-        console.log(this.precio)
-        this.precio = nuevoPrecio
-        document.writeln(`<p>Precio actualizado, el precio nuevo es: ${this.precio}</p>`)
-    }, 
-    mostrarEstado: () =>{
-         document.writeln(`<p>mostrar si el funko esta habilitado o no</p>`)
-    } 
-}
+            </ul>`);
+  },
+
+  actualizarPrecio: function (nuevoPrecio) {
+    console.log(this);
+    console.log(this.precio);
+    this.precio = nuevoPrecio;
+    document.writeln(
+      `<p>Precio actualizado, el precio nuevo es: ${this.precio}</p>`
+    );
+  },
+  mostrarTitulo: () => {
+    document.writeln(`<h2>Funko </h2>`);
+  },
+};
+
 // mostrar un objeto
 
-console.log(funko)
-document.writeln(funko)
+console.log(funko);
+document.writeln(funko);
 //mostrar las propiedades de un objeto
-document.writeln(`<p>Código: ${funko.codigo}</p>`)
-document.writeln(`<p>Nombre: ${funko.nombre}</p>`)
-document.writeln(`<p>Precio: $ ${funko['precio']}</p>`)
-document.writeln(`<p>Tamaño: ${funko.tamanio}</p>`)
-document.writeln(`<p>Tamaño: ${funko['tamanio']}</p>`)
-document.writeln(`<p>Marca: ${funko.marca}</p>`)
+document.writeln(`<p>Código: ${funko.codigo}</p>`);
+document.writeln(`<p>Nombre: ${funko.nombre}</p>`);
+document.writeln(`<p>Precio: $ ${funko["precio"]}</p>`);
+document.writeln(`<p>Tamaño: ${funko.tamanio}</p>`);
+document.writeln(`<p>Tamaño: ${funko["tamanio"]}</p>`);
+document.writeln(`<p>Marca: ${funko.marca}</p>`);
 //modificar la propiedad de un objeto
-funko.marca = 'Exclusive Funko'
-document.writeln(`<p>Marca: ${funko.marca}</p>`)
+funko.marca = "Exclusive Funko";
+document.writeln(`<p>Marca: ${funko.marca}</p>`);
 
 //agregar propiedades nuevas o metodos nuevos al objeto
-funko.tamanio = 'Regular'
-document.writeln(`<p>Tamaño: ${funko.tamanio}</p>`)
-console.log(funko)
+funko.tamanio = "Regular";
+document.writeln(`<p>Tamaño: ${funko.tamanio}</p>`);
+console.log(funko);
 
-funko.tienda = 'no definido'
-document.writeln(`<p>Tienda: ${funko.tienda}</p>`)
+funko.tienda = "no definido";
+document.writeln(`<p>Tienda: ${funko.tienda}</p>`);
 
 //borrar propiedad del objeto
-delete funko.tienda
-document.writeln(`<p>Tienda: ${funko.tienda}</p>`)
+delete funko.tienda;
+document.writeln(`<p>Tienda: ${funko.tienda}</p>`);
 
 //usar metodos del objeto
-funko.mostrarDatos()
-funko.actualizarPrecio(40)
-funko.mostrarDatos()
+funko.mostrarDatos();
+funko.actualizarPrecio(40);
+funko.mostrarTitulo()
+funko.mostrarDatos();
 
-console.log(this)
+console.log(this); //representa la ventana del navegador en este contexto
+
+console.log('Existe la propiedad codigo: '+ funko.hasOwnProperty('codigo'))
+console.log('Existe la propiedad descripcion: '+ funko.hasOwnProperty('descripcion'))
---
+++ js/09-clases.js
@@ -0,0 +1,44 @@
+//Usuarios
+// propiedades: Nombre, apellido, email, password, edad, dni, estado
+// metodos: iniciarSesion, recuperarPassword, cambiarEstado, mostrarDatos
+
+class Usuario{
+    //diseñar las propiedades
+    #email;
+    #password;
+    constructor(nombreParam, apellido, email, password, edad, dni){
+        // clave: valor
+        this.nombre = nombreParam
+        this.apellido = apellido
+        this.email = email
+        this.password = password
+        this.edad = edad
+        this.dni = dni
+        this.estado = true //propiedades por defecto
+    }
+    //diseñar los métodos
+    mostrarDatos(){
+        document.writeln(`<h3>Usuario</h3>`)
+        document.writeln(`<ul>
+            <li>Nombre y apellido: ${this.nombre, this.apellido} </li>
+            <li>Email: ${this.email} </li>
+            <li>Edad: ${this.edad} </li>
+            <li>DNI: ${this.dni} </li>
+            </ul>`)
+    }
+}
+
+//implementar o usar mi clase
+
+const marcos = new Usuario('Marcos', 'Molina', 'marcos@mail.com', '123asdA@sdfs', 25, '40222333')
+const valentina = new Usuario('Valentina', 'Iramain', 'valentina@mail.com', '123asdA@sdfs', 30, '35222333')
+
+console.log(marcos)
+marcos.mostrarDatos()
+console.log(valentina)
+valentina.mostrarDatos()
+
+document.writeln(`<p>Email de Marcos:  ${marcos.email}</p>`)
+marcos.email = 'marcosmolina@mail.com'
+document.writeln(`<p>Email de Marcos:  ${marcos.email}</p>`)
+marcos.mostrarDatos()
